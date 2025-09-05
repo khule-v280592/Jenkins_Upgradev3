e05131c9a4dc736afefc6dabd9aeaacdbb2849b0
khule-v280592 <khule.vivek92@outlook.com>
2025-09-05 17:53:56 +0530

    Update TestGreeter.java
    for jenkins training

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -20,12 +20,12 @@ public class TestGreeter {
     greeter = new Greeter();
   }
 
-  @Test
-  public void greetShouldIncludeTheOneBeingGreeted() {
-    String someone = "World";
+  // @Test
+  // public void greetShouldIncludeTheOneBeingGreeted() {
+  //   String someone = "World";
 
-    assertThat(greeter.greet(someone), containsString(someone));
-  }
+  //   assertThat(greeter.greet(someone), containsString(someone));
+  // }
   
 
   @Test
